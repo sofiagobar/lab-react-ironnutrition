d028a857e579806abef477512b3a179bc7c52906
Your Name <sofia.gobar@gmail.com>
2021-07-27 18:26:41 +0200

    Iterations 1&2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import FoodList from './components/food/foodlist/FoodList';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="root">
+        <div className="container">
+          <h1 className="title">IronNutrition</h1>
+          <div>
+            <input type="text" className="input search-bar" name="search" placeholder="Search" value=""/>
+          </div>
+          <div className="columns">
+            <div className="column">
+              <FoodList/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
